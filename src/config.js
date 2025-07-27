ceeba6507c4b28061f42cc248ae4d381b195c368
japhet john <japhetjohnk@gmail.com>
2025-07-27 16:58:31 +0000

    added config.js

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const CONFIG = {
+    API_KEY: import.meta.env.VITE_API_KEY || '2F4RzncerbMXhPvN6JjPGsegmAcnSg786uQWVzc6VNM2CFfjwqFLEJjJodSa5JRi4DXNDLUMgd9X7iR3yCFqmdx9KXv5FNJYMCr' // Fallback for local dev
+  };
